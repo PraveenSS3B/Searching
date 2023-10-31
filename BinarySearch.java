9df5676a34286bb415a6f9215b47502a78b09ed8
PraveenSS3B <praveenkumar66118@gmail.com>
2023-10-31 23:42:09 +0530

    T.C - O(logN) ; A.S - O(logN)

--- BinarySearch.java
+++ BinarySearch.java
@@ -5,9 +5,28 @@ public class BinarySearch {
 	public static void main(String[] args) {
 		int arr[] = { 2, 3, 4, 10 };
 
-		int key = 3;
+		int key = 10;
 
 		System.out.println(find(arr, arr.length, key));
+
+		System.out.println(find_Recursive(arr, 0, arr.length - 1, key));
+
+	}
+
+	private static int find_Recursive(int[] arr, int low, int high, int key) {
+
+		if (low > high)
+			return -1;
+
+		int mid = low + (high - low) / 2;
+
+		if (arr[mid] == key)
+			return mid;
+
+		else if (arr[mid] > key)
+			return find_Recursive(arr, low, mid - 1, key);
+
+		return find_Recursive(arr, mid + 1, high, key);
 
 	}
 
