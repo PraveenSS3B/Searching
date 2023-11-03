a154581bb6938aecab56ed6d062e3c8fd23a5bfc
PraveenSS3B <praveenkumar66118@gmail.com>
2023-11-03 23:38:54 +0530

    T.C - O(logn); A.S - O(logn)

--- LastOccurrenceIndexInSortedArray.java
+++ LastOccurrenceIndexInSortedArray.java
@@ -8,12 +8,38 @@ public class LastOccurrenceIndexInSortedArray {
 		int x = 10;
 
 		System.out.println(find_Naive(arr, arr.length, x));
+		
+		System.out.println(find_Recursive(arr, 0, arr.length - 1, x));
 
+	}
+
+	private static int find_Recursive(int[] arr, int low, int high, int x) {
+		
+		if(low > high)
+			return -1;
+		
+		int mid = low + (high - low) / 2;
+		
+		if(arr[mid] > x)
+			return find_Recursive(arr, low, mid - 1, x);
+		
+		else if(arr[mid] < x)
+			return find_Recursive(arr, mid + 1, high, x);
+		
+		else
+		{
+			if(mid == arr.length - 1 || arr[mid] != arr[mid + 1])
+				return mid;
+			
+			else
+				return find_Recursive(arr, mid + 1, high, x);
+		}
+		
 	}
 
 	private static int find_Naive(int[] arr, int n, int x) {
 
-		int index = 0;
+		int index = -1;
 
 		for (int i = 0; i < n; i++) {
 			if (arr[i] > x)
