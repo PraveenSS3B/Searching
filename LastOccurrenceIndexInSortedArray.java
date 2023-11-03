effe4a018e1f971404e31e62b04aa6831f55b85d
PraveenSS3B <praveenkumar66118@gmail.com>
2023-11-03 23:45:27 +0530

    Iterative soln -> T.C - O(logn); A.S - O(1)

--- LastOccurrenceIndexInSortedArray.java
+++ LastOccurrenceIndexInSortedArray.java
@@ -8,33 +8,59 @@ public class LastOccurrenceIndexInSortedArray {
 		int x = 10;
 
 		System.out.println(find_Naive(arr, arr.length, x));
-		
+
 		System.out.println(find_Recursive(arr, 0, arr.length - 1, x));
+
+		System.out.println(find_Iterative(arr, arr.length - 1, x));
+
+	}
+
+	private static int find_Iterative(int[] arr, int n, int x) {
+
+		int low = 0, high = n - 1;
+
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+
+			if (arr[mid] > x)
+				high = mid - 1;
+
+			else if (arr[mid] < x)
+				low = mid + 1;
+
+			else {
+				if (mid == n - 1 || arr[mid] != arr[mid + 1])
+					return mid;
+				else
+					low = mid + 1;
+			}
+		}
+
+		return -1;
 
 	}
 
 	private static int find_Recursive(int[] arr, int low, int high, int x) {
-		
-		if(low > high)
+
+		if (low > high)
 			return -1;
-		
+
 		int mid = low + (high - low) / 2;
-		
-		if(arr[mid] > x)
+
+		if (arr[mid] > x)
 			return find_Recursive(arr, low, mid - 1, x);
-		
-		else if(arr[mid] < x)
+
+		else if (arr[mid] < x)
 			return find_Recursive(arr, mid + 1, high, x);
-		
-		else
-		{
-			if(mid == arr.length - 1 || arr[mid] != arr[mid + 1])
+
+		else {
+			if (mid == arr.length - 1 || arr[mid] != arr[mid + 1])
 				return mid;
-			
+
 			else
 				return find_Recursive(arr, mid + 1, high, x);
 		}
-		
+
 	}
 
 	private static int find_Naive(int[] arr, int n, int x) {
