e6e8a79d406c5640d6a2d92c46813c91a2f95e4a
PraveenSS3B <praveenkumar66118@gmail.com>
2023-11-04 22:56:52 +0530

    Naive Soln T.C - O(n) ; A.S - O(1)

---
+++ Count1sInSortedArray.java
@@ -0,0 +1,23 @@
+package in.Searching;
+
+public class Count1sInSortedArray {
+
+	public static void main(String[] args) {
+		int arr[] = { 0, 0, 0, 1, 1, 1, 1 };
+
+		System.out.println(find_Naive(arr, arr.length));
+	}
+
+	private static int find_Naive(int[] arr, int n) {
+		int c = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == 1)
+				c++;
+		}
+
+		return c;
+
+	}
+
+}
