ea08428a931f8ad64fc678756cb55b740cfb0a76
PraveenSS3B <praveenkumar66118@gmail.com>
2023-11-04 23:00:03 +0530

    Better Soln T.C - O(n) ; A.S - O(1)

--- Count1sInSortedArray.java
+++ Count1sInSortedArray.java
@@ -6,6 +6,18 @@ public class Count1sInSortedArray {
 		int arr[] = { 0, 0, 0, 1, 1, 1, 1 };
 
 		System.out.println(find_Naive(arr, arr.length));
+		System.out.println(find_Better(arr, arr.length));
+	}
+
+	private static int find_Better(int[] arr, int n) {
+		int c = 0;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == 1) {
+				c = n - i;
+				break;
+			}
+		}
+		return c;
 	}
 
 	private static int find_Naive(int[] arr, int n) {
