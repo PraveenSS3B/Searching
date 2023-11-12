b88578af1600669a20b5a7edbd28446d42a2e2ae
PraveenSS3B <praveenkumar66118@gmail.com>
2023-11-12 12:59:45 +0530

    Naive Soln T.C - O(n); A.S - O(1)

---
+++ PeakElement.java
@@ -0,0 +1,32 @@
+package in.Searching;
+
+public class PeakElement {
+
+	public static void main(String[] args) {
+//		int arr[] = { 5, 10, 20, 15, 7 };
+
+		int arr[] = { 6, 7, 8, 20, 13 };
+
+		System.out.println(find_Naive(arr, arr.length));
+	}
+
+	private static int find_Naive(int[] arr, int n) {
+
+		if (n == 1)
+			return arr[0];
+
+		if (arr[0] >= arr[1])
+			return arr[0];
+
+		if (arr[n - 1] >= arr[n - 2])
+			return arr[n - 1];
+
+		for (int i = 1; i < n - 1; i++) {
+			if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+				return arr[i];
+
+		}
+		return -1;
+
+	}
+}
