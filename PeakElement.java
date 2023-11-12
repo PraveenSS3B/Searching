a81b7bcc3fce7088aa9a4fb68383d6173895600c
PraveenSS3B <praveenkumar66118@gmail.com>
2023-11-12 13:28:40 +0530

    Efficent Soln T.C - O(logn); A.S - O(1)

--- PeakElement.java
+++ PeakElement.java
@@ -5,9 +5,42 @@ public class PeakElement {
 	public static void main(String[] args) {
 //		int arr[] = { 5, 10, 20, 15, 7 };
 
-		int arr[] = { 6, 7, 8, 20, 13 };
+//		int arr[] = { 6, 7, 8, 20, 13 };
+
+		int arr[] = { 1, 2 };
 
 		System.out.println(find_Naive(arr, arr.length));
+
+		System.out.println(find_Efficient(arr, arr.length));
+	}
+
+	private static int find_Efficient(int[] arr, int n) {
+		if (n == 1)
+			return arr[0];
+
+		if (arr[0] >= arr[1])
+			return arr[0];
+
+		if (arr[n - 1] >= arr[n - 2])
+			return arr[n - 1];
+
+		int low = 1, high = n - 2;
+
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+
+			if (arr[mid] >= arr[mid - 1] && arr[mid] >= arr[mid + 1])
+				return arr[mid];
+
+			else if (arr[mid] < arr[mid + 1])
+				low = mid + 1;
+
+			else if (arr[mid - 1] > arr[mid])
+				high = mid - 1;
+
+		}
+
+		return -1;
 	}
 
 	private static int find_Naive(int[] arr, int n) {
