306de5929e31917c062851b19b39e32bf29ad9b4
PraveenSS3B <praveenkumar66118@gmail.com>
2023-11-14 23:44:01 +0530

    Better Soln T.C - O(n^2); A.S - O(1)

--- TripletInSortedArray.java
+++ TripletInSortedArray.java
@@ -3,11 +3,37 @@ package in.Searching;
 public class TripletInSortedArray {
 
 	public static void main(String[] args) {
-		int arr[] = { 2, 3, 4, 8, 9, 20, 40 };
+//		int arr[] = { 2, 3, 4, 8, 9, 20, 40 };
 
-		int x = 32;
+		int arr[] = { 2, 3, 5, 6, 15 };
+
+		int x = 20;
 
 		find_Naive(arr, arr.length, x);
+
+		find_Better(arr, arr.length, x);
+	}
+
+	private static void find_Better(int[] arr, int n, int x) {
+
+		for (int i = 0; i < n - 2; i++) {
+			int l = i + 1, h = n - 1;
+
+			while (l < h) {
+				int sum = arr[i] + arr[l] + arr[h];
+
+				if (sum == x) {
+					System.out.println("Triplets : " + arr[i] + ", " + arr[l] + ", " + arr[h]);
+					break;
+				}
+
+				if (sum > x)
+					h--;
+
+				else
+					l++;
+			}
+		}
 	}
 
 	private static void find_Naive(int[] arr, int n, int x) {
